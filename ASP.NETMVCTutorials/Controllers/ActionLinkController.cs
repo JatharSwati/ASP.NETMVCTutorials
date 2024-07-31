726d96897d62eb834cbba1758e043e362bb95ed2
JatharSwati <jatharswatiofficial@gmail.com>
2024-07-31 21:07:46 +0530

    10 | Action Link

---
+++ ASP.NETMVCTutorials/Controllers/ActionLinkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class ActionLinkController : Controller
+    {
+        // GET: ActionLink
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
