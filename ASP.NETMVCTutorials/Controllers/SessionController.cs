e16afbd2c8942915e22d3d6f032308cd92f8d38d
JatharSwati <jatharswatiofficial@gmail.com>
2024-07-31 20:23:45 +0530

    09 | Session

---
+++ ASP.NETMVCTutorials/Controllers/SessionController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class SessionController : Controller
+    {
+        // GET: Session
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Data comes from ViewData.";
+            ViewBag.Var2 = "Data comes from ViewBag.";
+            TempData["Var3"] = "Data comes from TempData.";
+            Session["Var4"] = "Data comes from Session.";
+
+            string[] students = { "Swati", "Pradip", "Shubhangi", "Pravin" };
+            Session["StudentsArray"] = students;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if(Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            if (Session["Var4"] != null)
+            {
+                Session["Var4"].ToString();
+            }
+            return View();
+        }
+    }
+}
