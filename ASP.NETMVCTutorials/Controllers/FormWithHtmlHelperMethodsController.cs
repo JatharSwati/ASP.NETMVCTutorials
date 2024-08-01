cdb521a0ad9f4715e44b83a58ae9cbf6b306bce1
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-01 14:12:39 +0530

    12 | Form with Html Helper Methods.

---
+++ ASP.NETMVCTutorials/Controllers/FormWithHtmlHelperMethodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class FormWithHtmlHelperMethodsController : Controller
+    {
+        // GET: FormWithHtmlHelperMethods
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
