ecd0c40b9bd7edbd0bf865bef93a2af307047d7d
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-01 11:56:51 +0530

    11 | Text Box

---
+++ ASP.NETMVCTutorials/Controllers/TextBoxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class TextBoxController : Controller
+    {
+        // GET: TextBox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
