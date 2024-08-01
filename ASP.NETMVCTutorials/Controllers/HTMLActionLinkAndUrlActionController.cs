799436a28ca21e472bec8a96b06269cc6931d8ce
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-01 16:35:25 +0530

    13 | Difference between HTML.ActionLink And Url.Action.

---
+++ ASP.NETMVCTutorials/Controllers/HTMLActionLinkAndUrlActionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class HTMLActionLinkAndUrlActionController : Controller
+    {
+        // GET: HTMLActionLinkAndUrlAction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
