32c9e0dee4a9f1ac42d5c447016269494d940f9c
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-02 22:14:27 +0530

    18 | Strongly Typed Partial View.

---
+++ ASP.NETMVCTutorials/Controllers/StronglyTypedPartialViewController.cs
@@ -0,0 +1,25 @@
+﻿using ASP.NETMVCTutorials.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class StronglyTypedPartialViewController : Controller
+    {
+        // GET: StronglyTypedPartialView
+
+        List<Product> ProductsList = new List<Product>()
+        {
+            new Product {Id = 1, Name = "Reebok Goggles", Price = 1000.00, Picture = "~/Images/Goggles.jpeg"},
+            new Product {Id = 2, Name = "Reebok Shoes", Price = 10000.00, Picture = "~/Images/Shoes.jpeg"},            
+            new Product {Id = 3, Name = "Reebok Watch", Price = 20000.00, Picture = "~/Images/Watch.jpeg"}
+        };
+        public ActionResult Index()
+        {
+            return View(ProductsList);
+        }
+    }
+}
---
+++ ASP.NETMVCTutorials/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NETMVCTutorials.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Double Price { get; set; }
+
+        public string Picture { get; set; }
+    }
+}
