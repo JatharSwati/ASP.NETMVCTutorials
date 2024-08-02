5d5db3f468d7b5988be877cbc71cba7e4a992e6c
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-02 19:02:56 +0530

    16 | Partial View

---
+++ ASP.NETMVCTutorials/Controllers/PartialViewController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
