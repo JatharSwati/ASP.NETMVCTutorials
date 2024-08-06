3f9a00cbe1d332d9c56cdb4b23a2251578b17827
JatharSwati <jatharswatiofficial@gmail.com>
2024-08-06 19:14:57 +0530

    21 | Data Annotation

---
+++ ASP.NETMVCTutorials/Controllers/DataAnnotationController.cs
@@ -0,0 +1,31 @@
+﻿using ASP.NETMVCTutorials.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NETMVCTutorials.Controllers
+{
+    public class DataAnnotationController : Controller
+    {
+        // GET: DataAnnotation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Customer customer)
+        {
+            if(ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script>alert('Data has been Submited !')</script";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ ASP.NETMVCTutorials/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NETMVCTutorials.Models
+{
+    public class Customer
+    {
+        [Required(ErrorMessage = "Id is Mandatory")]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Name is Mandatory")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Age is Mandatory")]
+        public int? CustomerAge { get; set; }
+
+        [Required(ErrorMessage = "Gender is Mandatory")]
+        public string CustomerGender { get; set; }
+
+        [Required(ErrorMessage = "Email is Mandatory")]
+        public string CustomerEmail { get; set; }
+    }
+}
