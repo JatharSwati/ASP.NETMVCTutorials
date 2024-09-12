1267897ceec3b12bb374141fcea4fee2d91e1fb6
JatharSwati <jatharswatiofficial@gmail.com>
2024-09-12 22:05:10 +0530

    47 | Creating SignUp OR Register Page With Login.

---
+++ CreatingSignUpORRegisterPageWithLogin/Controllers/LoginController.cs
@@ -0,0 +1,65 @@
+﻿using CreatingSignUpORRegisterPageWithLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreatingSignUpORRegisterPageWithLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        SignupLoginEntities db = new SignupLoginEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User u)
+        {
+            var user = db.Users.Where(model => model.username == u.username && model.password == u.password).FirstOrDefault();
+            if (User != null)
+            {
+                Session["UserId"] = u.id.ToString();
+                Session["Username"] = u.username.ToString();
+                TempData["LoginSuccessfulMessage"] = "<script>alert('Login Successful !')</script>";
+                return RedirectToAction("Index", "User");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or password is incorrect !')</script>";
+            }
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(User u)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Users.Add(u);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registered Successfully !')</script>";
+                    ModelState.Clear();
+                }
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registeration Failed !')</script>";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CreatingSignUpORRegisterPageWithLogin/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CreatingSignUpORRegisterPageWithLogin.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            if (Session["UserId"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                return View();
+            }            
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
