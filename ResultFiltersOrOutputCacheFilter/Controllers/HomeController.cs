3c9b3513c09dc99690c1bb659b82b0d8d2ec73fe
JatharSwati <jatharswatiofficial@gmail.com>
2024-09-15 11:53:57 +0530

    52 | Result Filters | Output Cache Filter.

---
+++ ResultFiltersOrOutputCacheFilter/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using ResultFiltersOrOutputCacheFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResultFiltersOrOutputCacheFilter.Controllers
+{
+    public class HomeController : Controller    {
+
+        PersonDBEntities db = new PersonDBEntities();
+
+        // GET: Home
+
+        [OutputCache(Duration = 20, Location = System.Web.UI.OutputCacheLocation.Client)]
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [OutputCache(Duration = 60)]
+        public ActionResult GetData()
+        {
+            var data = db.People.ToList();
+            return View(data);
+        }
+    }
+}
