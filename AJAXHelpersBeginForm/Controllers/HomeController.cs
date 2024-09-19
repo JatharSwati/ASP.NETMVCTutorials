4510dd5405eed06ebf043914dfbeb1849045a4a9
JatharSwati <jatharswatiofficial@gmail.com>
2024-09-19 20:44:37 +0530

    53  & 54 | AJAX In ASP.NET MVC | Introduction To AJAX and 55 & 56 | AJAX Helpers In ASP.NET MVC | Ajax.BeginForm() Method of AJAX.

---
+++ AJAXHelpersBeginForm/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AJAXHelpersBeginForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AJAXHelpersBeginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeDatabaseEntities db = new EmployeeDatabaseEntities();
+
+        // GET: Home
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Employee e)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Employees.Add(e);
+                int a = db.SaveChanges();
+                if (a > 0)
+                {
+                    return JavaScript("alert('Data Inserted !')");
+                }
+                else
+                {
+                    return JavaScript("alert('Data Not Inserted !')");
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ AJAXHelpersBeginForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AJAXHelpersBeginForm.MvcApplication" Language="C#" %>
